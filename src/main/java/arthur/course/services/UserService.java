faa7959e0fe29ed912aa7306d0a3c4395f024b44
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-21 12:31:42 -0300

    Exception handling - delete

--- src/main/java/arthur/course/services/UserService.java
+++ src/main/java/arthur/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import arthur.course.entities.User;
 import arthur.course.repositories.UserRepository;
+import arthur.course.services.exceptions.DatabaseException;
 import arthur.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
