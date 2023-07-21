3c5aef41b01cd2eea754a6572e691d371d2719eb
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-21 11:39:52 -0300

    [200~Exception handling - findById

--- src/main/java/arthur/course/services/UserService.java
+++ src/main/java/arthur/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import arthur.course.entities.User;
 import arthur.course.repositories.UserRepository;
+import arthur.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
